c27c112659a6c74f34b59807f49ce6cde8fafab0
Martín Villanueva <martin.villanueva@usm.cl>
2020-03-08 13:04:52 -0300

    Updates package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='mahts',
-    version='0.0.1',
+    version='0.0.2',
     author='Martín Villanueva',
     author_email='nallivam@gmail.com',
     packages=find_packages(),
