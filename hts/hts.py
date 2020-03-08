b61d5b5e7b182352e65057910678940cb07a0ac8
Martín Villanueva <martin.villanueva@usm.cl>
2020-03-08 00:57:19 -0300

    Adds top-down and bottom-up approaches

--- hts/hts.py
+++ hts/hts.py
@@ -1,43 +1,65 @@
 import numpy as np
 import pandas as pd
-
-from tree import build_tree, get_nodes_per_level
+from anytree import LevelOrderIter
+from hts.tree import (build_tree, 
+                      get_nodes_per_level,
+                      compute_summing_matrix)
 
 class HierarchicalTimeSerie:
-    def __init__(self, base_series, hierarchy, method, forecast_method):
+    def __init__(self, hierarchy):
         """
         Parameters
         ----------
-        base_series: pandas.Dataframe 
-            Dataframe containing the base series in its columns
         hierarchy: dict 
-            Dictionary descriptor of the hierarchy of series
-        method: string 
-            Hierarchical time series forecasting method
-        forecast_method: string
-            Forecast method to use (arima, and ets).
+            Dictionary describing the hierarchical relationship between
+            the time series.
         """
-        self.base_series = base_series
+        tree = build_tree(hierarchy)
+        summing_matrix,tree_nodes,bottom_nodes = compute_summing_matrix(tree)
         self.hierarchy = hierarchy
-        self.method = method
-        self.forecast_method = forecast_method
-        self.tree = build_tree(hierarchy)
-        
-        # aggregating the series for the rest of the hierarchy
-        nodes_per_level = get_nodes_per_level(self.tree, skip_leaves=True)
-        hierarchy_series = pd.DataFrame()
-        for level in nodes_per_level[::-1]:
-            for node in level:
-            hierarchy_series[node] = bottom_series.loc[:, hierarchy[node]].sum(axis=1)
-        self.hierarchy_series = hierarchy_series
-        
+        self.tree = tree
+        self.summing_matrix = summing_matrix
+        self.tree_nodes = tree_nodes
+        self.bottom_nodes = bottom_nodes
+        self.proportions = None
 
-    def compute_average_hist_proportions():
-        pass
+    def compute_bottom_up(self, forecast):
+        assert set(forecast.columns) == set(self.bottom_nodes), \
+            f"'forecast' dataframe must have only the columns: {self.bottom_nodes}."
+        result = np.dot(forecast.loc[:, self.bottom_nodes].values, self.summing_matrix.T)
+        return pd.DataFrame(result, columns=self.tree_nodes)
 
-    def computer_hist_average_proportios():
-        pass
+    def compute_top_down(self, data, forecast, kind="ahp"):
+        assert set(data.columns) == set(self.bottom_nodes), \
+            f"'data' dataframe must have only the columns: {self.bottom_nodes}."
+        assert set(forecast.columns) == set(["root"]), \
+            "'forecast' dataframe must have only the column: 'root'."
+        data = data.copy(deep=True).loc[:, self.bottom_nodes]
+        root_timeserie = data.values.sum(axis=1)
+        if kind == "ahp":
+            proportions = np.mean(data.values / root_timeserie.reshape(-1,1), axis=0)
+        elif kind == "pha":
+            proportions = data.values.mean(axis=0) / root_timeserie.mean()
+        forecast_bottom = pd.DataFrame(forecast["root"].values.reshape(-1,1) * proportions.reshape(1,-1),
+                                       columns=self.bottom_nodes)
+        self.proportions = proportions
+        return self.compute_bottom_up(forecast_bottom)
+    
+    def compute_forecasted_proportions(self, forecast):
+        assert set(forecast.columns) == set(self.tree_nodes), \
+            f"'forecast' dataframe must have only the columns: {self.tree_nodes}."
+        proportions_by_level = pd.DataFrame(np.ones(forecast.shape[0]), columns=["root"])
+        for node in LevelOrderIter(self.tree):
+            if node.name == "root": continue
+            level_nodes = [nd.name for nd in node.parent.children]
+            proportions_by_level[node.name] = forecast.loc[:,node.name]/forecast.loc[:,level_nodes].sum(axis=1)
+            proportions_by_level[node.name] *= proportions_by_level[node.parent.name]
+        forecast_bottom = proportions_by_level[self.bottom_nodes] * forecast["root"].values.reshape(-1,1)
+        self.proportions = proportions_by_level
+        return self.compute_bottom_up(forecast_bottom)
 
-    def forecast():
+    def compute_middle_out(self, data, forecast):
         pass
     
+    def compute_optimal_combination(self, forecast):
+        pass
