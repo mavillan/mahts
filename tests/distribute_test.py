744e4c77935c0764973b0f21f4f61e7299014673
Martín Villanueva <martin.villanueva@usm.cl>
2020-03-09 21:13:23 -0300

    Adds two more tests

--- tests/distribute_test.py
+++ tests/distribute_test.py
@@ -44,7 +44,48 @@ class TestForecaster(unittest.TestCase):
         pd.testing.assert_frame_equal(actual, expected)
 
     def test_it_computes_top_down_with_ahp(self):
-        pass
+        data = pd.DataFrame(
+            [[2, 1, 2, 1, 2],
+             [2, 2, 2, 2, 2]],
+            columns = ["A1", "A2", "B1", "B2", "B3"]
+        )
+        forecast = pd.DataFrame(
+            [[10], 
+             [20]], 
+            columns = ["root"] 
+        )
+        expected = pd.DataFrame(
+            [[10., 3.875, 6.125, 2.25, 1.625, 2.25, 1.625, 2.25],
+             [20., 7.75, 12.25, 4.5, 3.25, 4.5, 3.25, 4.5]],
+            columns = ["root", "A", "B", "A1", "A2", "B1", "B2", "B3"]
+        )
+
+        hts = HTSDistributor(self.hierarchy)
+        actual = hts.compute_top_down(data, forecast, kind="ahp")
+
+        pd.testing.assert_frame_equal(actual, expected)
+
+    def test_it_computes_top_down_with_pha(self):
+        data = pd.DataFrame(
+            [[3, 5, 3, 4, 2],
+             [1, 3, 5, 4, 2]],
+            columns = ["A1", "A2", "B1", "B2", "B3"]
+        )
+        forecast = pd.DataFrame(
+            [[10], 
+             [20]], 
+            columns = ["root"] 
+        )
+        expected = pd.DataFrame(
+            [[10., 3.75, 6.25, 1.25, 2.5, 2.5, 2.5, 1.25],
+             [20., 7.5, 12.5, 2.5, 5., 5., 5., 2.5]],
+            columns = ["root", "A", "B", "A1", "A2", "B1", "B2", "B3"]
+        )
+
+        hts = HTSDistributor(self.hierarchy)
+        actual = hts.compute_top_down(data, forecast, kind="pha")
+
+        pd.testing.assert_frame_equal(actual, expected)
     
     def test_it_computes_forecast_proportions(self):
         pass
