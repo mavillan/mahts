4f3d23134fc462f56d22e1277ddaee23c2a6022a
Martín Villanueva <martin.villanueva@walmart.com>
2020-05-17 12:38:45 -0500

    Improves message output for lsmr backend

--- mahts/utils.py
+++ mahts/utils.py
@@ -13,8 +13,14 @@ def format_lstsq_output(output, backend):
         print(f"x-norm: {output[8]}")
     
     if backend == "lsmr":
-        stop_mapping = {1: "x is an approximate solution to Ax = b.",
-                        2: "x approximately solves the least-squares problem."}
+        stop_mapping = {0: "0. x=0 is a solution.",
+                        1: "1. x is an approximate solution to A*x = B, according to atol and btol.",
+                        2: "2. x approximately solves the least-squares problem according to atol.",
+                        3: "3. COND(A) seems to be greater than CONLIM.",
+                        4: "4. is the same as 1 with atol = btol = eps (machine precision).",
+                        5: "5. is the same as 2 with atol = eps.",
+                        6: "6. is the same as 3 with CONLIM = 1/eps.",
+                        7: "7. means ITN reached maxiter before the other stopping conditions were satisfied."}
         print(f"stop: {stop_mapping[output[1]]}")
         print(f"n-iterations: {output[2]}")
         print(f"r1norm norm(r): {output[3]}")
